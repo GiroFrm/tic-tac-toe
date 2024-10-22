2ba85577a2293a1c3baa7af04bfa81baddfd38c7
GiroFrm <girofram844@gmail.com>
2024-10-22 18:16:49 +1100

    feat: implement GameController for managing game flow
    - Define Cell function to create cell objects with getValue and addToken methods.
    - Implement GameController to manage game state and player turns.
    - Initialize game board using Gameboard function.
    - Define players with names and tokens.
    - Add switchPlayerTurn method to alternate active player.
    - Add getActivePlayer method to retrieve the current active player.
    - Add printNewRound method to display the board and indicate the active player's turn.
    - Implement playRound method to handle token placement, switch turns, and print the new round state.
    - Initialize game with printNewRound to display the initial state.

--- script.js
+++ script.js
@@ -31,5 +31,65 @@ function Gameboard() {
 }
 
 
+function Cell() {
+    let value =0;
 
+    const getValue = () => value;
+
+    const addToken = (player) => {
+        value =player;
+    }
+
+    return {addToken, getValue}
+
+}
+
+function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+   
+    const board =Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            token: 1
+        },
+        {
+            name: playerTwoName,
+            token: 2
+        }
+    ]
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = ()=> { 
+        activePlayer = activePlayer === players[0] ? players[1] : players[0]
+   }
+
+   const getActivePlayer = () => activePlayer;
+
+   const printNewRound = () =>{
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`)
+   }
+
+   const playRound = (column, row) => {
+
+     console.log( `Dropping ${getActivePlayer().name}'s token into column ${column}, row${row}`);
+     board.dropToken(column, row, getActivePlayer().token)
+
+     switchPlayerTurn();
+     printNewRound();
+
+   };
+
+   printNewRound();
+
+   return {
+    playRound,
+    getActivePlayer
+   }
+  
+}
+
+const game = GameController();
 
